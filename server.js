5075fbc257d7ca9fd02254b6ba3c319be30b0a46
Arthur Borges <arthurborges.dev@gmail.com>
2023-04-14 20:34:01 -0300

    Make initial integration with GPT3.5 chat API and test by sending a message

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
-/* express server scaffold */
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Configuration, OpenAIApi } from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+function loadOpenAI() {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  const openai = new OpenAIApi(configuration);
+  return openai;
+}
+
+app.use(bodyParser.json())
+
+app.post('/action', async (req, res) => {
+  const openai = loadOpenAI();
+  const { message } = req.body;
+  
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: message}],
+    });
+    console.log(completion.data.choices[0].message);
+    res.send(completion.data.choices[0].message);
+  } catch (e) {
+    console.error(e);
+    res.send(e);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
