91324a4c7c98e71c123b770384da3e2eb6ed573a
Arthur Borges <arthurborges.dev@gmail.com>
2023-04-25 17:21:23 -0300

    WIP - Create express modules to handle weaviate requests and started integrate GPT queries with weaviate

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import loadOpenAI from './loadOpenAI.js';
 import dotenv from 'dotenv';
-import { vectorizer } from './weaviate.js';
+import weaviate from './weaviate/index.js';
+import fs from 'fs';
+
+import { vectorize, getObject } from './weaviate/utils.js';
 
 dotenv.config();
 
@@ -11,24 +14,45 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json())
 
+app.use('/weaviate', weaviate);
+
+let prompt = '';
+
+  try {
+    const data = fs.readFileSync('prompt.txt', 'utf8');
+    prompt = data.toString();
+  } catch(e) {
+    console.log('Error:', e.stack);
+  }
+
 app.post('/action', async (req, res) => {
   const openai = loadOpenAI();
   const { name, message } = req.body;
 
-  const gptMessage = {
+  const systemMessage = {
+    role: "system",
+    content: prompt,
+  }
+
+  const userMessage = {
     role: "user",
-    content: `${name}: ${message}`,
+    name: name,
+    content: message,
   }
 
   try {
+    const weaviateObjects = await getObject('Context', 'name role content') || [];
+    const context = weaviateObjects?.data?.Get?.Context || [];
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
-        gptMessage,
+        systemMessage,
+        ...context,
+        userMessage,
       ],
     });
-    vectorizer('Action', gptMessage);
-    vectorizer('Response', completion.data.choices[0].message);
+    vectorize('Context', userMessage);
+    vectorize('Context', completion.data.choices[0].message);
     res.send(completion.data.choices[0].message);
   } catch (e) {
     console.error(e);
